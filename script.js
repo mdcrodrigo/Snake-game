07cf6e9bb4c7fe847e09bb4b7faff9f5d0e64f87
mdcrodrigo <mdcrodrigo@hotmail.com>
2022-01-22 15:58:56 -0300

    Finalizado

--- script.js
+++ script.js
@@ -40,10 +40,19 @@ function update (event){
 }
 
 function iniciarJogo (){
+
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(')
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -65,9 +74,7 @@ function iniciarJogo (){
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-   /* snake.pop();*/
-
-    let newHead = {
+      let newHead = {
         x: snakeX,
         y: snakeY     
     }
